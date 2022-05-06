e52f0c13b1db8abc16796972dd7adeb9475ffa12
Tiffany Frierson <QuantumP7@gmail.com>
2022-05-06 19:03:55 -0400

    draft5

--- src/App.js
+++ src/App.js
@@ -154,84 +154,92 @@ function App() {
 
   return (
     <main className="main-container">
-      <h2 className="headline"><span className="headline-gradient">Bank Contract Project</span> 💰</h2>
+      <h2 className="headline">
+        <span className="headline-gradient">Meme Coin Project</span>
+        <img className="inline p-3 ml-2" src="https://i.imgur.com/5JfHKHU.png" alt="Meme Coin" width="60" height="30" />
+      </h2>
       <section className="customer-section px-10 pt-5 pb-10">
         {error && <p className="text-2xl text-red-700">{error}</p>}
         <div className="mt-5">
-          {currentBankName === "" && isBankerOwner ?
-            <p>"Setup the name of your bank." </p> :
-            <p className="text-3xl font-bold">{currentBankName}</p>
-          }
+          <span className="mr-5"><strong>Coin:</strong> {tokenName} </span>
+          <span className="mr-5"><strong>Ticker:</strong>  {tokenSymbol} </span>
+          <span className="mr-5"><strong>Total Supply:</strong>  {tokenTotalSupply}</span>
         </div>
         <div className="mt-7 mb-9">
           <form className="form-style">
             <input
               type="text"
-              className="input-style"
+              className="input-double"
               onChange={handleInputChange}
-              name="deposit"
-              placeholder="0.0000 ETH"
-              value={inputValue.deposit}
+              name="walletAddress"
+              placeholder="Wallet Address"
+              value={inputValue.walletAddress}
             />
-            <button
-              className="btn-purple"
-              onClick={deposityMoneyHandler}>Deposit Money In ETH</button>
-          </form>
-        </div>
-        <div className="mt-10 mb-10">
-          <form className="form-style">
             <input
               type="text"
-              className="input-style"
+              className="input-double"
               onChange={handleInputChange}
-              name="withdraw"
-              placeholder="0.0000 ETH"
-              value={inputValue.withdraw}
+              name="transferAmount"
+              placeholder={`0.0000 ${tokenSymbol}`}
+              value={inputValue.transferAmount}
             />
             <button
               className="btn-purple"
-              onClick={withDrawMoneyHandler}>
-              Withdraw Money In ETH
-            </button>
+              onClick={transferToken}>Transfer Tokens</button>
           </form>
         </div>
-        <div className="mt-5">
-          <p><span className="font-bold">Customer Balance: </span>{customerTotalBalance}</p>
-        </div>
-        <div className="mt-5">
-          <p><span className="font-bold">Bank Owner Address: </span>{bankOwnerAddress}</p>
-        </div>
-        <div className="mt-5">
-          {isWalletConnected && <p><span className="font-bold">Your Wallet Address: </span>{customerAddress}</p>}
-          <button className="btn-connect" onClick={checkIfWalletIsConnected}>
-            {isWalletConnected ? "Wallet Connected 🔒" : "Connect Wallet 🔑"}
-          </button>
-        </div>
-      </section>
-      {
-        isBankerOwner && (
-          <section className="bank-owner-section">
-            <h2 className="text-xl border-b-2 border-indigo-500 px-10 py-4 font-bold">Bank Admin Panel</h2>
-            <div className="p-10">
+        {isTokenOwner && (
+          <section>
+            <div className="mt-10 mb-10">
               <form className="form-style">
                 <input
                   type="text"
                   className="input-style"
                   onChange={handleInputChange}
-                  name="bankName"
-                  placeholder="Enter a Name for Your Bank"
-                  value={inputValue.bankName}
+                  name="burnAmount"
+                  placeholder={`0.0000 ${tokenSymbol}`}
+                  value={inputValue.burnAmount}
                 />
                 <button
-                  className="btn-grey"
-                  onClick={setBankNameHandler}>
-                  Set Bank Name
+                  className="btn-purple"
+                  onClick={burnTokens}>
+                  Burn Tokens
+                </button>
+              </form>
+            </div>
+            <div className="mt-10 mb-10">
+              <form className="form-style">
+                <input
+                  type="text"
+                  className="input-style"
+                  onChange={handleInputChange}
+                  name="mintAmount"
+                  placeholder={`0.0000 ${tokenSymbol}`}
+                  value={inputValue.mintAmount}
+                />
+                <button
+                  className="btn-purple"
+                  onClick={mintTokens}>
+                  Mint Tokens
                 </button>
               </form>
             </div>
           </section>
-        )
-      }
+        )}
+        <div className="mt-5">
+          <p><span className="font-bold">Contract Address: </span>{contractAddress}</p>
+        </div>
+        <div className="mt-5">
+          <p><span className="font-bold">Token Owner Address: </span>{tokenOwnerAddress}</p>
+        </div>
+        <div className="mt-5">
+          {isWalletConnected && <p><span className="font-bold">Your Wallet Address: </span>{yourWalletAddress}</p>}
+          <button className="btn-connect" onClick={checkIfWalletIsConnected}>
+            {isWalletConnected ? "Wallet Connected 🔒" : "Connect Wallet 🔑"}
+          </button>
+        </div>
+
+      </section>
     </main>
   );
 }
